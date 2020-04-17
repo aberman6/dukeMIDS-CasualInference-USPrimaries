e52650b8b00a40a99f2a7ad869a74eee7d5be040
aeb100 <aeb100@duke.edu>
2020-04-17 13:52:02 -0400

    updated cleaning file to fix election type variab;e

--- 10_code/cleaning_2.R
+++ 10_code/cleaning_2.R
@@ -1,36 +1,38 @@
 library(dplyr)
+library(tidyr)
 
 # Load Data
 turnout <- read.csv('20_intermediate_files/turnout_v2.csv', row.names = 1, stringsAsFactors = FALSE)
+election_format <- read.csv('00_source_data/electiontype.csv')
 
-treatment_group <- c('Colorado','Maine', 'Minnesota', 'Idaho', 'Washington')
+# Long election_format dataset
+election_format <- election_format %>%
+    mutate(State = trimws(State)) %>%
+    gather(key = 'Year', value = 'Format', -State) %>%
+    mutate(Year = as.numeric(gsub('X','', Year)))
+
+# States that has caucuses in 2008-2016, and had a primary in 2020
+treatment_group <- c('Colorado','Maine', 'Minnesota', 'Washington')
 
 # States that are excluded from the analysis
+# States with unique formats
+# States that switched back and forth
+# States effected by COVID
 exclude_group <- c('California', 'Hawaii', 'Alaska', 'Ohio', 'Wisconsin', 
                    'Connecticut', 'Delaware','District of Columbia', 'Georgia',
-                   'Indiana', 'Kansas', 'Kentucky', 'Louisiana', 'Maryland',
-                   'Montana', 'Nebraska', 'New Jersey', 'New Mexico', 'New York',
-                   'Pennsylvania', 'Oregon', 'Rhode Island','Wyoming', 
-                   'West Virginia')
-
-# Clean Idaho 2008 - seemd to have 2 primary days
-idaho_2008_clean <- turnout %>%
-    mutate(State = trimws(State)) %>%
-    filter(State == 'Idaho' & Year == 2008) %>%
-    mutate(Democrat = '64,026') %>%
-    slice(-1)
+                   'Idaho', 'Indiana', 'Kansas', 'Kentucky', 'Louisiana', 
+                   'Maryland', 'Montana', 'Nebraska', 'New Jersey', 'New Mexico', 
+                   'New York', 'Pennsylvania', 'Oregon', 'Rhode Island',
+                   'Wyoming', 'West Virginia')
 
 # Removes states with strange systems and states effected by COVID
-# Cleans Idaho 2008
 # Creates treatment variable
 # Formats columns correctly
 # Removes records with NAs for outcome variable
+# Corrects for incorrect election type variable
 turnout_clean <- turnout %>%
     # Exclude states
     mutate(State = trimws(State)) %>%
-    # Idaho 2008 remove duplicate
-    filter(State != 'Idaho' | Year != 2008) %>%
-    rbind(idaho_2008_clean) %>%
     filter(!State %in% exclude_group) %>%
     # Create numeric treatment variable
     # treatment == 1 : State that switched from from caucus to primary in 2020
@@ -46,7 +48,13 @@ turnout_clean <- turnout %>%
            VEP_Counted = as.numeric(gsub('%', '', VEP_Counted))) %>%
     # Democratic votes only drop NaNs 
     mutate(VEP_Counted_D = Democrat/VEP) %>%
-    filter(!is.na(VEP_Counted_D))
+    filter(!is.na(VEP_Counted_D)) %>%
+    # Correct for election type errors
+    left_join(election_format, by = c("State", "Year"))  %>%
+    mutate(Type = Format, Pop = Total) %>%
+    # Remove extraneous columns
+    select(State, Year, treatment, VEP_Counted_D, VEP, 
+           Democrat, Type, Pop, White, Black, Hispanic)
 
 # Now that we have the first pass of cleaning, what states have complete data?
 # States with complete data from 2018, 2016, & 2020
@@ -63,12 +71,6 @@ complete_states
 turnout_clean <- turnout_clean %>%
     filter(Year != 2012) %>%
     filter(State %in% complete_states)
-
-# Keep only columns we care about
-turnout_clean <- turnout_clean %>%
-    mutate(Pop = Total) %>%
-    select(State, Year, treatment, VEP_Counted_D, VEP, 
-           Democrat, Type, Pop)
 
 # Export cleaned dataset
 write.csv(turnout_clean, '20_intermediate_files/turnout_v3.csv')
